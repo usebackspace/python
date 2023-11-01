818f245488554af8039149dbbae68093c08129fe
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2023-11-01 18:19:13 +0530

    updated

--- 15.while loop.py
+++ 15.while loop.py
@@ -15,33 +15,3 @@ while(i<4):
         j+=1
 
 
-
-n=5
-while(n):
-    n=n-1
-    print(n,end=' ')
-
-
-
-t=(1,2,4,3,8,9)
-print([t[i] for i in range(0,len(t),2)])
-
-
-for k in  range(1,101):
-    if int(i*0.5)==k*0.5:
-        print(i)
-
-print([k for k in range(1,101) if int(i*0.5)==k*0.5])
-
-
-
-print('------------------------')
-
-x=50
-def f(x):
-    print('x ix',x)
-    x=2
-    print('x in fuc',x)
-print("cahnage x to",x)
-f(x)
-print('x ix now ',x)
