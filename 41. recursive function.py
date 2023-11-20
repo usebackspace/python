f0de306314e8ac81e2c40d57342d3161f38116d8
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2023-11-20 16:01:34 +0530

    updated

--- 41. recursive function.py
+++ 41. recursive function.py
@@ -1,3 +1,4 @@
+print('---- Factorial, Using Recursive Function ----')
 def factorial(n):
     # Base case: factorial of 0 or 1 is 1
     if n == 0 or n == 1:
@@ -5,3 +6,19 @@ def factorial(n):
     else:
         # Recursive case: n! = n * (n-1)!
         return n * factorial(n - 1)
+
+n=5
+print(f'factorial of {n} is : {factorial(n)}')
+
+print()
+
+#=======================================================================================
+print('---- Fibonacci, Using Recursive Function ----')
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+n=5
+print(f'Fibonacci series of {n} is : {fibonacci(n)}')
--- fibonacci serires.py
+++ fibonacci serires.py
@@ -3,20 +3,9 @@ n2=1
 n3=0
 x= int(input("Enter the Number: "))
 print(n1,n2,end=',')
-for i in range(2,x):
+for i in range(1,x):
     n3=n1+n2
     n1=n2
     n2=n3
     print(n3,end=',')
 
-def fibonacci_iterative(n):
-    fib_series = [0, 1]
-
-    for i in range(2, n):
-        fib_series.append(fib_series[i-1] + fib_series[i-2])
-
-    return fib_series
-
-# Example: Generate Fibonacci series up to 10 terms
-result = fibonacci_iterative(10)
-print(result)
