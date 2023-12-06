ac6862c90add3399098e03bae9e86099d87204fd
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2023-12-06 20:48:05 +0530

    update

--- demo.py
+++
@@ -1,8 +0,0 @@
-# Example with numeric keys
-numeric_dict = {1: 'one', 2: 'two', 3: 'three'}
-
-# Example with tuple keys
-tuple_dict = {('a', 1): 'value1', ('b', 2): 'value2'}
-
-print(numeric_dict)
-print(tuple_dict[('a',1)])
